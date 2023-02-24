13cb00da594e70ac10b4611afcb153352dc91783
Sabitha26 <124423680+Sabitha26@users.noreply.github.com>
2023-02-24 12:19:53 +0530

    Add files via upload

---
+++ Capstone Project/js/main.js
@@ -0,0 +1,13 @@
+//home page-js code
+ function redirectToServicesPage() {
+    window.location.href = './services.html';
+}
+  window.addEventListener('load', () => {
+    exploreButtonHandler();
+  });
+function redirectToLoginPage() {
+    window.location.href = './login.html';
+}
+  window.addEventListener('load', () => {
+    loginButtonHandler();
+  });
